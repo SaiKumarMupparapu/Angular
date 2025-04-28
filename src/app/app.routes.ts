118f1e24fb795c4c41eafa95c84420c142e11a94
saiKumarMupparapu <mupparapusaismart@gmail.com>
2025-04-28 18:27:55 +0530

    404 Page not found added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginForRouteComponent } from './login-for-route/login-for-route.compon
 import { AboutForRouteComponent } from './about-for-route/about-for-route.component';
 import { ContactForRouteComponent } from './contact-for-route/contact-for-route.component';
 import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginForRouteComponent},
     {path:'about',component:AboutForRouteComponent},
     {path:'contact',component:ContactForRouteComponent},
-    {path:'',component:HomeComponent}
+    {path:'',component:HomeComponent},
+    
+    {path:'**',component:PageNotFoundComponent}
 ];
