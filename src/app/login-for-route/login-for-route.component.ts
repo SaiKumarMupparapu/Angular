af9e3af5729db802d3ac3ac4e8cf5092d7e9c95c
saiKumarMupparapu <mupparapusaismart@gmail.com>
2025-04-29 20:59:52 +0530

    reactive form added

--- src/app/login-for-route/login-for-route.component.ts
+++ src/app/login-for-route/login-for-route.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-for-route',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-for-route.component.html',
   styleUrl: './login-for-route.component.css'
 })
 export class LoginForRouteComponent {
+  userName=new FormControl('kumar');
+  password=new FormControl('123456789');
+  msg:string='';
+
+    checkLogin(){
+      if(this.userName.value=='S@i' && this.password.value=="123abc" ){
+        this.msg='Welcome ';
+      }else{
+        this.msg='invalid credentials';
+      }
+  }
+  getInfo(){
+        console.log(this.userName.value);
+        console.log(this.password.value);
+  }
+  setValue(){
+    this.userName.setValue('Kali');
+    this.password.setValue('time');
+  }
 
 }
