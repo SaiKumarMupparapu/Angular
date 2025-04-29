0050353a5129f425a0279b95ded702729a78e65f
saiKumarMupparapu <mupparapusaismart@gmail.com>
2025-04-29 21:50:03 +0530

    form grouping added

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { routes } from '../app.routes';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
@@ -13,14 +14,29 @@ export class ProfileComponent {
   userName:string | null ='';
   constructor(private route:ActivatedRoute){}
 
-  ngOnInit() {
+  // ngOnInit() {
   //  this.userName= this.route.snapshot.paramMap.get('name');
   // this.route.queryParams.subscribe(param=>{
   //   this.userName=param['name'];
   // })
-  this.route.data.subscribe(param=>{
-    this.userName=param['name'];
+  // this.route.data.subscribe(param=>{
+  //   this.userName=param['name'];
+  // })
+  // }
+
+  profileFormGroup=new FormGroup({
+    name:new FormControl('name'),
+    pswd:new FormControl('password'),
+    email:new FormControl('email')
   })
+
+  submitData(){
+    console.log(this.profileFormGroup.value);
   }
 
+  setData(){
+    this.profileFormGroup.setValue(
+      {name:'kali',pswd:'time',email:'123'}
+    );
+  }
 }
